402817ae96b1da31caf69f3d71340a174b28bdf7
Anvesh1007 <akanvesh681@gmail.com>
2025-04-09 01:26:37 +0530

    Profile of Admin Methods

---
+++ Frontend/src/Page/Admin/Profile.jsx
@@ -0,0 +1,100 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { setUserData } from "../../redux/actions";
+import { baseApiURL } from "../../baseUrl";
+import toast from "react-hot-toast";
+const Profile = () => {
+  const [showPass, setShowPass] = useState(false);
+  const router = useLocation();
+  const [data, setData] = useState();
+  const dispatch = useDispatch();
+  const [password, setPassword] = useState({
+    new: "",
+    current: "",
+  });
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(
+        `${baseApiURL()}/${router.state.type}/details/getDetails`,
+        { employeeId: router.state.loginid },
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        if (response.data.success) {
+          setData(response.data.user[0]);
+          dispatch(
+            setUserData({
+              fullname: `${response.data.user[0].firstName} ${response.data.user[0].middleName} ${response.data.user[0].lastName}`,
+              semester: response.data.user[0].semester,
+              enrollmentNo: response.data.user[0].enrollmentNo,
+              branch: response.data.user[0].branch,
+            })
+          );
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [dispatch, router.state.loginid, router.state.type]);
+
+  const checkPasswordHandler = (e) => {
+    e.preventDefault();
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(
+        `${baseApiURL()}/admin/auth/login`,
+        { loginid: router.state.loginid, password: password.current },
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        if (response.data.success) {
+          changePasswordHandler(response.data.id);
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+        console.error(error);
+      });
+  };
+
+  const changePasswordHandler = (id) => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .put(
+        `${baseApiURL()}/admin/auth/update/${id}`,
+        { loginid: router.state.loginid, password: password.new },
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        if (response.data.success) {
+          toast.success(response.data.message);
+          setPassword({ new: "", current: "" });
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+        console.error(error);
+      });
+  };
+}
