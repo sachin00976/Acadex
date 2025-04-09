06b4e4ab85f049974ac76e3016d1448c021568bb
sachin00976 <162875619+sachin00976@users.noreply.github.com>
2025-04-09 19:24:09 +0530

    feat: add AddFaculty, EditFaculty, AddStudent, and EditStudent components with form handling and API integration

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { createBrowserRouter,createRoutesFromElements,Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
+import AddFaculty from "./Screens/Faculty/AddFaculty.jsx"
 const Router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Login/>}/>
+         <Route path="/" element={<Login/>}/>,
+        // <Route path="/" element={<AddFaculty/>}/>
+        
     )
 )
 
