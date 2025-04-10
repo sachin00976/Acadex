e0276b00f851ba34a617e2ed7248e9c86cb04b85
Sahil Singh <sahilsingh1892005@gmail.com>
2025-04-10 14:14:33 +0530

    add heading component

--- Frontend/src/components/Heading.jsx
+++ Frontend/src/components/Heading.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Heading = () => {
   return (
-    <div>
-      
+    <div className="flex justify-between items-center w-full">
+      <div className="font-semibold text-3xl border-l-8 border-red-500 pl-3"  >
+        {props.title}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Heading
