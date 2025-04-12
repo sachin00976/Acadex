a7e1ca7036d781295f540bcb44f02f3b50ff785d
Anvesh1007 <akanvesh681@gmail.com>
2025-04-13 00:59:42 +0530

    Material page in Faculty

--- Frontend/src/Screens/Faculty/Material.jsx
+++ Frontend/src/Screens/Faculty/Material.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { FiUpload } from "react-icons/fi";
+import Heading from "../../components/Heading";
+import { AiOutlineClose } from "react-icons/ai";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { baseApiURL } from "../../baseUrl";
 const Material = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { fullname } = useSelector((state) => state.userData);
+  const [subject, setSubject] = useState();
+  const [file, setFile] = useState();
+  const [selected, setSelected] = useState({
+    title: "",
+    subject: "",
+    faculty: fullname.split(" ")[0] + " " + fullname.split(" ")[2],
+  });
+
+  useEffect(() => {
+    toast.loading("Loading Subjects");
+    axios
+      .get(`${baseApiURL()}/subject/getSubject`)
+      .then((response) => {
+        toast.dismiss();
+        if (response.data.success) {
+          setSubject(response.data.subject);
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.dismiss();
+        toast.error(error.message);
+      });
+  }, []);
+
+  const addMaterialHandler = () => {
+    toast.loading("Adding Material");
+    const headers = {
+      "Content-Type": "multipart/form-data",
+    };
+    const formData = new FormData();
+    formData.append("title", selected.title);
+    formData.append("subject", selected.subject);
+    formData.append("faculty", selected.faculty);
+    formData.append("type", "material");
+    formData.append("material", file);
+    axios
+      .post(`${baseApiURL()}/material/addMaterial`, formData, {
+        headers: headers,
+      })
+      .then((response) => {
+        toast.dismiss();
+        if (response.data.success) {
+          toast.success(response.data.message);
+          setSelected({
+            title: "",
+            subject: "",
+            faculty: fullname.split(" ")[0] + " " + fullname.split(" ")[2],
+          });
+          setFile("");
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.dismiss();
+        toast.error(error.response.data.message);
+      });
+  };}
 
 export default Material
