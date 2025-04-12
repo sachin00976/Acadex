b62bbe4a5048505f21610f8934b5acba48d42493
Saurabh kumar <1213saurabhsingh1213@gmail.com>
2025-04-12 22:09:18 +0530

    Resolved merge conflict in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import Router from "./Router.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
-=======
-//import MainLayout from "./Screens/layout/MainLayout.jsx";
->>>>>>> 6bb6ce198d984179721fe33152d15c2af40464b9
+// import MainLayout from "./Screens/layout/MainLayout.jsx";
 import Login from "./components/Login.jsx";
 import Admin from "./Screens/Admin/Admin.jsx";
 
 function App() {
   return (
-
-    <RouterProvider  router={Router}/>
+    <RouterProvider router={Router} />
   );
 }
 
