93c47847c5f7a7b787c430a9f77905f184e0202d
Saurabh kumar <1213saurabhsingh1213@gmail.com>
2025-04-12 22:08:04 +0530

    WIP: changes in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Router from "./Router.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./Screens/layout/MainLayout.jsx";
 import Login from "./components/Login.jsx";
 import Admin from "./Screens/Admin/Admin.jsx";
 
