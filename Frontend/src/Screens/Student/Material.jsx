cb2876c875fcaf23f8c2a7c8639d01360bb5edaa
Anvesh1007 <akanvesh681@gmail.com>
2025-04-14 01:24:55 +0530

    Material methods in Student

--- Frontend/src/Screens/Student/Material.jsx
+++ Frontend/src/Screens/Student/Material.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Heading from "../../components/Heading";
+import { IoMdLink } from "react-icons/io";
+import { HiOutlineCalendar, HiOutlineSearch } from "react-icons/hi";
+import toast from "react-hot-toast";
+import { baseApiURL } from "../../baseUrl";
 const Material = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [subject, setSubject] = useState();
+  const [selected, setSelected] = useState();
+  const [material, setMaterial] = useState([]);
+  useEffect(() => {
+    toast.loading("Loading Subjects");
+    axios
+      .get(`${baseApiURL()}/subject/getSubject`)
+      .then((response) => {
+        toast.dismiss();
+        if (response.data.success) {
+          setSubject(response.data.subject);
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.dismiss();
+        toast.error(error.message);
+      });
+  }, []);
+
+  const getSubjectMaterial = () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(
+        `${baseApiURL()}/material/getMaterial`,
+        { subject: selected },
+        { headers }
+      )
+      .then((response) => {
+        if (response.data.success) {
+          setMaterial(response.data.material);
+        } else {
+          // Error
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const onSelectChangeHandler = (e) => {
+    setMaterial();
+    setSelected(e.target.value);
+  };
 }
 
 export default Material
