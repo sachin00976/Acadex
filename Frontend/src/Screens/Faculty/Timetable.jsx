1cd66db364a4745734e2c4765edc8e273f45d096
Anvesh1007 <akanvesh681@gmail.com>
2025-04-14 00:47:36 +0530

    Timetable in Faculty

--- Frontend/src/Screens/Faculty/Timetable.jsx
+++ Frontend/src/Screens/Faculty/Timetable.jsx
@@ -77,5 +77,97 @@ const Timetable = () => {
 
         toast.error(error.response.data.message);
       });
-  };}
+  };
+  return (
+    <div className="w-full mx-auto mt-10 flex justify-center items-start flex-col mb-10">
+      <div className="flex justify-between items-center w-full">
+        <Heading title={`Upload Timetable`} />
+      </div>
+      <div className="w-full flex justify-evenly items-center mt-12">
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          <p className="mb-4 text-xl font-medium">Add Timetable</p>
+          <select
+            id="branch"
+            className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] accent-blue-700 mt-4"
+            value={addselected.branch}
+            onChange={(e) =>
+              setAddSelected({ ...addselected, branch: e.target.value })
+            }
+          >
+            <option defaultValue>-- Select Branch --</option>
+            {branch &&
+              branch.map((branch) => {
+                return (
+                  <option value={branch.name} key={branch.name}>
+                    {branch.name}
+                  </option>
+                );
+              })}
+          </select>
+          <select
+            onChange={(e) =>
+              setAddSelected({ ...addselected, semester: e.target.value })
+            }
+            value={addselected.semester}
+            name="branch"
+            id="branch"
+            className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] accent-blue-700 mt-4"
+          >
+            <option defaultValue>-- Select Semester --</option>
+            <option value="1">1st Semester</option>
+            <option value="2">2nd Semester</option>
+            <option value="3">3rd Semester</option>
+            <option value="4">4th Semester</option>
+            <option value="5">5th Semester</option>
+            <option value="6">6th Semester</option>
+            <option value="7">7th Semester</option>
+            <option value="8">8th Semester</option>
+          </select>
+          {!addselected.link && (
+            <label
+              htmlFor="upload"
+              className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] mt-4 flex justify-center items-center cursor-pointer"
+            >
+              Select Timetable
+              <span className="ml-2">
+                <FiUpload />
+              </span>
+            </label>
+          )}
+          {previewUrl && (
+            <p
+              className="px-2 border-2 border-blue-500 py-2 rounded text-base w-[80%] mt-4 flex justify-center items-center cursor-pointer"
+              onClick={() => {
+                setFile("");
+                setPreviewUrl("");
+              }}
+            >
+              Remove Selected Timetable
+              <span className="ml-2">
+                <AiOutlineClose />
+              </span>
+            </p>
+          )}
+          <input
+            type="file"
+            name="upload"
+            id="upload"
+            accept="image/*"
+            hidden
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-blue-500 text-white mt-8 px-4 py-2 rounded-sm"
+            onClick={addTimetableHandler}
+          >
+            Add Timetable
+          </button>
+          {previewUrl && (
+            <img className="mt-6" src={previewUrl} alt="timetable" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Timetable
