65c15b8c840ce67e4eab99ec76af294de28b578d
Anvesh1007 <akanvesh681@gmail.com>
2025-04-17 02:11:39 +0530

    Complete profile page of Student

--- Frontend/src/Screens/Student/Profile.jsx
+++ Frontend/src/Screens/Student/Profile.jsx
@@ -97,6 +97,79 @@ const Profile = () => {
         console.error(error);
       });
   };
-}
+  return (
+    <div className="w-full mx-auto my-8 flex justify-between items-start">
+      {data && (
+        <>
+          <div>
+            <p className="text-2xl font-semibold">
+              Hello {data.firstName} {data.middleName} {data.lastName}👋
+            </p>
+            <div className="mt-3">
+              <p className="text-lg font-normal mb-2">
+                Enrollment No: {data.enrollmentNo}
+              </p>
+              <p className="text-lg font-normal mb-2">Branch: {data.branch}</p>
+              <p className="text-lg font-normal mb-2">
+                Semester: {data.semester}
+              </p>
+              <p className="text-lg font-normal mb-2">
+                Phone Number: +91 {data.phoneNumber}
+              </p>
+              <p className="text-lg font-normal mb-2">
+                Email Address: {data.email}
+              </p>
+            </div>
+            <button
+              className={`${
+                showPass ? "bg-red-100 text-red-600" : "bg-blue-600 text-white"
+              }  px-3 py-1 rounded mt-4`}
+              onClick={() => setShowPass(!showPass)}
+            >
+              {!showPass ? "Change Password" : "Close Change Password"}
+            </button>
+            {showPass && (
+              <form
+                className="mt-4 border-t-2 border-blue-500 flex flex-col justify-center items-start"
+                onSubmit={checkPasswordHandler}
+              >
+                <input
+                  type="password"
+                  value={password.current}
+                  onChange={(e) =>
+                    setPassword({ ...password, current: e.target.value })
+                  }
+                  placeholder="Current Password"
+                  className="px-3 py-1 border-2 border-blue-500 outline-none rounded mt-4"
+                />
+                <input
+                  type="password"
+                  value={password.new}
+                  onChange={(e) =>
+                    setPassword({ ...password, new: e.target.value })
+                  }
+                  placeholder="New Password"
+                  className="px-3 py-1 border-2 border-blue-500 outline-none rounded mt-4"
+                />
+                <button
+                  className="mt-4 hover:border-b-2 hover:border-blue-500"
+                  onClick={checkPasswordHandler}
+                  type="submit"
+                >
+                  Change Password
+                </button>
+              </form>
+            )}
+          </div>
+          <img
+            src={process.env.REACT_APP_MEDIA_LINK + "/" + data.profile}
+            alt="student profile"
+            className="h-[240px] w-[240px] object-cover rounded-lg shadow-md"
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Profile
