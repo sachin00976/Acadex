7faf8e9452f126b72516a8f66a79e4ada94477d9
Anvesh1007 <akanvesh681@gmail.com>
2025-04-17 00:52:14 +0530

    Marks of Student

--- Frontend/src/Screens/Student/Marks.jsx
+++ Frontend/src/Screens/Student/Marks.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import Heading from "../../components/Heading";
+import { baseApiURL } from "../../baseUrl";
 
 const Marks = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const userData = useSelector((state) => state.userData);
+  const [internal, setInternal] = useState();
+  const [external, setExternal] = useState();
 
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(
+        `${baseApiURL()}/marks/getMarks`,
+        { enrollmentNo: userData.enrollmentNo },
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        if (response.data.length !== 0) {
+          setInternal(response.data.Mark[0].internal);
+          setExternal(response.data.Mark[0].external);
+        }
+      })
+      .catch((error) => {
+        toast.dismiss();
+        console.log(error);
+      });
+  }, [userData.enrollmentNo]);
+}
 export default Marks
