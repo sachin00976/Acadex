a0886096b07d9ac5b9f18b9d86990f77fc42a69b
Anvesh1007 <akanvesh681@gmail.com>
2025-04-18 03:45:11 +0530

    Complete TimeTable page of Student

--- Frontend/src/Screens/Student/TimeTable.jsx
+++ Frontend/src/Screens/Student/TimeTable.jsx
@@ -34,5 +34,36 @@ const Timetable = () => {
     };
     userData && getTimetable();
   }, [userData, userData.branch, userData.semester]);
-}
+
+  return (
+    <div className="w-full mx-auto mt-10 flex justify-center items-start flex-col mb-10">
+      <div className="flex justify-between items-center w-full">
+        <Heading title={`Timetable of Semester ${userData.semester}`} />
+        {timetable && (
+          <p
+            className="flex justify-center items-center text-lg font-medium cursor-pointer hover:text-red-500 hover:scale-110 ease-linear transition-all duration-200 hover:duration-200 hover:ease-linear hover:transition-all"
+            onClick={() =>
+              window.open(process.env.REACT_APP_MEDIA_LINK + "/" + timetable)
+            }
+          >
+            Download
+            <span className="ml-2">
+              <FiDownload />
+            </span>
+          </p>
+        )}
+      </div>
+      {timetable && (
+        <img
+          className="mt-8 rounded-lg shadow-md w-[70%] mx-auto"
+          src={process.env.REACT_APP_MEDIA_LINK + "/" + timetable}
+          alt="timetable"
+        />
+      )}
+      {!timetable && (
+        <p className="mt-10">No Timetable Available At The Moment!</p>
+      )}
+    </div>
+  );
+};
 export default TimeTable
